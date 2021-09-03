bf1a050135f2d42cf03b5e79af87290439222bef
Lahia Ardhan <lahia.ardhanm@gmail.com>
2021-09-03 10:38:20 +0700

    Tugas 6 React JS
    Tugas ini saya kumpulkan kepada Tim Niomic

---
+++ src/Lib/DaftarMinuman.js
@@ -0,0 +1,29 @@
+const DaftarMinuman = [
+   {
+      Nama: "Es Cendol",
+      Harga: "10.000",
+      img: "https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-cendol.jpg"
+   },
+   {
+      Nama: "Es Pisang Hijau",
+      Harga: "10.000",
+      img: "https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-pisang-hijau.jpg"
+   },
+   {
+      Nama: "Es Kacang Merah",
+      Harga: "10.000",
+      img: "https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-kacang-merah.jpg"
+   },
+   {
+      Nama: "Es Pleret",
+      Harga: "10.000",
+      img: "https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-pleret.jpg"
+   },
+   {
+      Nama: "Es Legen",
+      Harga: "10.000",
+      img: "https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-legen.jpg"
+   }
+];
+
+export default DaftarMinuman;
